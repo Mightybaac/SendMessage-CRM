a0de6c55f73197f2f837cc70ea305d645a478100
Mightybaac <Millarchristopherlee@gmail.com>
2023-01-29 19:42:53 -0500

    Update CRM.cpp
    Constants: In the updated code, the names of the CRM and other applications have been defined as constants and can be easily changed by updating their values in a single location. This makes the code easier to maintain and reduces the likelihood of making a mistake when updating the code.
    
    Function: The updated code has a function, named SendTextToWindow, that performs the functionality of sending text to a window. This function abstracts the underlying logic and makes the code more readable and easier to maintain.
    
    Error handling: The updated code has a function, named CopyTextToClipboard, that performs the functionality of copying text to the clipboard and returns a boolean indicating whether the operation was successful or not. This allows for better error handling and makes it easier to understand the code.
    
    Cleaner code: The updated code is more readable, maintainable and modular as it separates different functionalities into different functions. This makes it easier for other developers to understand and modify the code if necessary.

--- CRM.cpp
+++ CRM.cpp
@@ -2,10 +2,39 @@
 #include <string>
 #include <Windows.h>
 
+const char* CRM_APPLICATION = "CRM_Application";
+const char* OTHER_APPLICATION = "Other_Application";
+
+void SendTextToWindow(const char* windowName, const std::string& text) {
+    HWND hwnd = FindWindow(NULL, windowName);
+    if (hwnd != NULL) {
+        SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)text.c_str());
+    } else {
+        std::cout << windowName << " application not found." << std::endl;
+    }
+}
+
+bool CopyTextToClipboard(const std::string& text) {
+    if (OpenClipboard(NULL)) {
+        HGLOBAL hglbCopy;
+        char* buffer;
+        EmptyClipboard();
+        hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (text.size() + 1) * sizeof(char));
+        buffer = (char*)GlobalLock(hglbCopy);
+        strcpy_s(buffer, text.size() + 1, text.c_str());
+        GlobalUnlock(hglbCopy);
+        SetClipboardData(CF_UNICODETEXT, hglbCopy);
+        CloseClipboard();
+        return true;
+    } else {
+        std::cout << "Failed to open clipboard" << std::endl;
+        return false;
+    }
+}
+
 int main() {
     std::string input;
     bool keepRunning = true;
-    
     while (keepRunning) {
         std::cout << "Enter text to be written in the CRM and copied to another application (Enter 'q' to quit): ";
         std::getline(std::cin, input);
@@ -15,44 +44,13 @@ int main() {
             continue;
         }
 
-        // Write the text to the CRM
-        // Assume the CRM application is open and active
-        // Replace "CRM_Application" with the actual name of the CRM application
-        HWND crm = FindWindow(NULL, "CRM_Application");
-        if (crm != NULL) {
-            // Send the text to the CRM's input field
-            SendMessage(crm, WM_SETTEXT, 0, (LPARAM)input.c_str());
-        } else {
-            std::cout << "CRM application not found." << std::endl;
+        SendTextToWindow(CRM_APPLICATION, input);
+
+        if (!CopyTextToClipboard(input)) {
+            continue;
         }
 
-        // Copy the text to the clipboard
-        // This will allow the text to be pasted in another application
-        if (OpenClipboard(NULL)) {
-            HGLOBAL hglbCopy;
-            char* buffer;
-
-            EmptyClipboard();
-            hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (input.size() + 1) * sizeof(char));
-            buffer = (char*)GlobalLock(hglbCopy);
-            strcpy_s(buffer, input.size() + 1, input.c_str());
-            GlobalUnlock(hglbCopy);
-            SetClipboardData(CF_UNICODETEXT, hglbCopy);
-            CloseClipboard();
-        } else {
-            std::cout << "Failed to open clipboard" << std::endl;
-        }
-
-        // Send the text to another application
-        // Assume the other application is open and active
-        // Replace "Other_Application" with the actual name of the other application
-        HWND other = FindWindow(NULL, "Other_Application");
-        if (other != NULL) {
-            // Send the text to the other application's input field
-            SendMessage(other, WM_SETTEXT, 0, (LPARAM)input.c_str());
-        } else {
-            std::cout << "Other application not found." << std::endl;
-        }
+        SendTextToWindow(OTHER_APPLICATION, input);
     }
 
     return 0;
